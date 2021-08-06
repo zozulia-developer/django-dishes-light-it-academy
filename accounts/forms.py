1084356b8116a47013236d49d8c87bcb0cf07df0
zozulia-developer <kirillzozulya95@gmail.com>
2021-08-06 17:50:49 +0300

    ru localization

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,23 +1,28 @@
 from django.core.exceptions import ValidationError
 from django.forms import ModelForm, Form, CharField, PasswordInput, TextInput, EmailInput
 from django.contrib.auth import get_user_model
+from django.utils.translation import ugettext_lazy as _
 
 
 class SignUpForm(ModelForm):
     username = CharField(
         max_length=30,
+        label=_('Username'),
         widget=TextInput(attrs={'class': 'form-control'})
     )
     email = CharField(
         max_length=40,
+        label=_('Email'),
         widget=EmailInput(attrs={'class': 'form-control'})
     )
     password = CharField(
         max_length=40,
+        label=_('Password'),
         widget=PasswordInput(attrs={'class': 'form-control'})
     )
     confirm_password = CharField(
         max_length=40,
+        label=_('Confirm password'),
         widget=PasswordInput(attrs={'class': 'form-control'})
     )
 
@@ -38,9 +43,11 @@ class SignUpForm(ModelForm):
 class LoginRawForm(Form):
     username = CharField(
         max_length=30,
+        label=_('Username'),
         widget=TextInput(attrs={'class': 'form-control'})
     )
     password = CharField(
         max_length=40,
+        label=_('Password'),
         widget=PasswordInput(attrs={'class': 'form-control'})
     )
